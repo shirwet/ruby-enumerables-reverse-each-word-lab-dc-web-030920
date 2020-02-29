9ffc704d1bf00faab6ed7899dfbdc06ae4e48439
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-29 22:13:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,4 @@ def reverse_each_word(sentence1)
 arr = sentence1.split (" ")
 new_arr = arr.collect {|a| a.reverse}
 new_arr.join(" ")
-  sentence1.reverse
-end
-
-arr = str.split(" ")
-  new_arr = arr.collect{|a| a.reverse}
-  new_arr.join(" ")
+  end
